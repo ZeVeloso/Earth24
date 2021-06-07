a58f9bbaec6602bdf4b9aba8765b5b26481f6545
ZeVeloso <veloso.j04@gmail.com>
2021-06-07 22:04:33 +0000

    trabalho final

--- Earth24/models/BingMapObjects.cs
+++ Earth24/models/BingMapObjects.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace Earth24.models
-{
-    public class Location
-    {
-        public float latitude;
-        public float longitude;
+//namespace Earth24.models
+//{
+//    public class Location
+//    {
+//        public float latitude;
+//        public float longitude;
 
-        public Location(float lat, float lon)
-        {
-            latitude = lat;
-            longitude = lon;
-        }
-        public Location()
-        {
-            latitude = 0;
-            longitude = 0;
-        }
-    }
+//        public Location(float lat, float lon)
+//        {
+//            latitude = lat;
+//            longitude = lon;
+//        }
+//        public Location()
+//        {
+//            latitude = 0;
+//            longitude = 0;
+//        }
+//    }
 
-    public class Pushpin
-    {
-        public string color;
-        public Location location;
-    }
+//    public class Pushpin
+//    {
+//        public string color;
+//        public Location location;
+//    }
 
-    public class PolygonOptions
-    {
-        public string fillColor;
-        public string strokeColor;
-        public int strokeThickness;
-    }
+//    public class PolygonOptions
+//    {
+//        public string fillColor;
+//        public string strokeColor;
+//        public int strokeThickness;
+//    }
 
-    public class Polygon
-    {
-        public Location[][] rings;
-        public PolygonOptions options;
-    }
-}
+//    public class Polygon
+//    {
+//        public Location[][] rings;
+//        public PolygonOptions options;
+//    }
+//}
